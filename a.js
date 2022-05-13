ccb70e674797de9f960b2f37d0e2fec5f7c495b9
doongu <must1080@naver.com>
2022-05-13 19:19:41 +0900

    change modules

--- a.js
+++ a.js
@@ -1,8 +1,8 @@
-//import axios from 'axios'
-//import cheerio from 'cheerio'
+import axios from 'axios'
+import cheerio from 'cheerio'
 
-const axios2 = require("axios");
-const cheerio2 = require("cheerio");
+//const axios2 = require("axios");
+//const cheerio2 = require("cheerio");
 
 axios2.get("http://example.com/")
     .then((response) => {
