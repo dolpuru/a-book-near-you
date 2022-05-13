a89d3d03e15ef003758aff7952f2ef8ce3b67135
doongu <must1080@naver.com>
2022-05-13 20:46:19 +0900

    test

--- a.js
+++ a.js
@@ -1,13 +1,34 @@
-// import axios2 from 'axios'
-// import cheerio2 from 'cheerio'
+// // import axios2 from 'axios'
+// // import cheerio2 from 'cheerio'
 
-const axios2 = require("axios");
-const cheerio2 = require("cheerio");
+// const axios2 = require("axios");
+// const cheerio2 = require("cheerio");
 
-axios2.get("http://example.com/")
+axios.get("http://example.com/")
     .then((response) => {
         const htmlString = response.data;
-        const $ = cheerio2.load(htmlString);
+        const $ = cheerio.load(htmlString);
         const data = $('h1').text();
         console.log(data);
-    })
+    })       
+//  axios
+
+//         .get("https://jsonplaceholder.typicode.com/todos/1")
+
+//         .then(function (result) {
+
+//         console.log("통신 결과 : ", result);
+
+//         console.log("status : ", result.status);
+
+//         console.log("data : ", result.data);
+
+//         })
+
+//         .catch(function (error) {
+
+//         console.log("에러 발생 : ", error);
+
+//         });
+
+//         console.log("바로 실행 로그");
