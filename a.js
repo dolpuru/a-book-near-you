7076186762024ca9c1b130f52f1013fe52c094b9
doongu <must1080@naver.com>
2022-05-13 18:27:00 +0900

    test

---
+++ a.js
@@ -0,0 +1,12 @@
+// import axios from 'axios'
+// import cheerio from 'cheerio'
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+axios.get("http://example.com/")
+    .then((response) => {
+        const htmlString = response.data;
+        const $ = cheerio.load(htmlString);
+        const data = $('h1').text();
+        console.log(data);
+    })
