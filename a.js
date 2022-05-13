ffe687842e89ddceba00c2eef06ebfa31216512d
doongu <must1080@naver.com>
2022-05-13 21:11:34 +0900

    test

--- a.js
+++ a.js
@@ -1,8 +1,9 @@
-// // import axios2 from 'axios'
-// // import cheerio2 from 'cheerio'
+// import axios2 from 'axios'
+// const cheerio = require("cheerio");
+// import cheerio from 'cheerio'
+// const cheerio = require("cheerio");
 
-// const axios2 = require("axios");
-// const cheerio2 = require("cheerio");
+import cheerio from 'cheerio'
 
 axios.get("http://example.com/")
     .then((response) => {
@@ -10,7 +11,21 @@ axios.get("http://example.com/")
         const $ = cheerio.load(htmlString);
         const data = $('h1').text();
         console.log(data);
-    })       
+    })
+
+
+// // import cheerio2 from 'cheerio'
+
+// const axios2 = require("axios");
+// const cheerio2 = require("cheerio");
+
+// axios.get("http://example.com/")
+//     .then((response) => {
+//         const htmlString = response.data;
+//         const $ = cheerio.load(htmlString);
+//         const data = $('h1').text();
+//         console.log(data);
+//     })       
 //  axios
 
 //         .get("https://jsonplaceholder.typicode.com/todos/1")
