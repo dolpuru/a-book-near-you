a08b4d8a4a872aba7ca29b73a4ace6341ba95335
doongu <must1080@naver.com>
2022-05-13 20:04:01 +0900

    test

--- a.js
+++ a.js
@@ -1,8 +1,8 @@
-import axios2 from 'axios'
-import cheerio2 from 'cheerio'
+// import axios2 from 'axios'
+// import cheerio2 from 'cheerio'
 
-//const axios2 = require("axios");
-//const cheerio2 = require("cheerio");
+const axios2 = require("axios");
+const cheerio2 = require("cheerio");
 
 axios2.get("http://example.com/")
     .then((response) => {
