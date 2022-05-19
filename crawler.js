78515b3a9024938cdc4a3dd1ebd9252332cacf32
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2022-05-19 17:26:30 +0900

    upload_tester

--- crawler.js
+++ crawler.js
@@ -92,12 +92,12 @@ console.log("바로 실행 로그");
 // console.log("바로 실행 로그");
 
 // yes24 매점들 불러오는거
-// axios.get("http://www.yes24.com/Mall/UsedStore/Detail/Seomyeon").then(function (result) {
-//     console.log("통신 결과 : ", result);
-// }).catch(function (error) {
-//     console.log("에러 발생 : ", error);
-// });
-// console.log("바로 실행 로그");
+axios.get("http://www.yes24.com/Mall/UsedStore/Detail/Seomyeon").then(function (result) {
+    console.log("통신 결과 : ", result);
+}).catch(function (error) {
+    console.log("에러 발생 : ", error);
+});
+console.log("바로 실행 로그");
 
 
 // 각 매점들 영어이름 XXXXX에 넣으면됨. 
